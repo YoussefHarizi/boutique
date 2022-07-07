b8e180b1c41eb15e73251bb6bdbffefb69b45a81
Harizi <yharizi@gticanada.com>
2022-07-07 09:30:09 -0400

    fix password hasher

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,23 +8,30 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     private $entityManager;
+
+
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager=$entityManager;
     }
+
+
     #[Route('/register', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request,UserPasswordHasherInterface  $encoder): Response
     {
         $user=new User();
         $form=$this->createForm(RegisterType::class);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
             $user=$form->getData();
+            $password=$encoder->hashPassword($user,$user->getPassword());
+            dd($password);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
         }
