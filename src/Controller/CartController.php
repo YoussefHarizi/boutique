3a7e20771f46ce012dd642d5462ca69f6a8382be
Harizi <yharizi@gticanada.com>
2022-07-13 13:14:29 -0400

    mon panier -template et controller-

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,18 +3,38 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager=$entityManager;
+    }
+
     #[Route('/mon-panier', name: 'app_panier')]
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
+        // dd($cart->get());
+        $fullCarts=[];
+        foreach ($cart->get() as $id => $quantity) {
+            $fullCarts[]=[
+                'product'=>$this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity'=>$quantity
+
+            ];
+            
+        }
+        // dd($fullCarts);
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'fullCarts' => $fullCarts,
         ]);
     }
 
