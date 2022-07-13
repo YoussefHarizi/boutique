b1bc85f7b4154fa13326c7d0641178a1e7d533c1
Harizi <yharizi@gticanada.com>
2022-07-13 11:17:48 -0400

    classe cart et enregistrement de tableau cart dans session

---
+++ src/Controller/CartController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/mon-panier', name: 'app_panier')]
+    public function index(Cart $cart): Response
+    {
+        dd($cart->get());
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_add_to_cart')]
+    public function add(Cart $cart, $id): Response
+    {
+       $cart->add($id);
+       return $this->redirectToRoute('app_panier');
+    }
+
+    #[Route('/cart/remove', name: 'app_remove_cart')]
+    public function remove(Cart $cart): Response
+    {
+       $cart->remove();
+       return $this->redirectToRoute('app_products');
+    }
+}
